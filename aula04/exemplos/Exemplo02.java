3bacd458360e0e28d1ee09652c7d972ee89feffb
Shirley1317 <estevesshirley@gmail.com>
2021-12-02 11:55:50 -0300

    aula 4 - parte 1

---
+++ aula04/exemplos/Exemplo02.java
@@ -0,0 +1,23 @@
+public class Exemplo02 {
+public static void main(String[] args) {
+
+    // printf( FORMATO, DADOS)
+    // FORMATO:
+    // %d = inteiro
+    // %f = decimal
+    // %s = texto
+    // \n = pula linha
+
+    // %.2f = 2 casas decimais com arredondamento
+
+    System.out.printf("o número é %d\n", 10);
+
+    System.out.printf("os números são %d e %d\n", 10, 15);
+
+    System.out.printf("a média é %f\n", 45.6754321);
+
+    System.out.printf("a média é %2f\n", 45.6754321);
+    
+}
+    
+}
