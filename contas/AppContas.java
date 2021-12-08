8d199ab61f2b4d7996f68f629a6ad1bba95a634a
Shirley1317 <estevesshirley@gmail.com>
2021-12-08 15:58:58 -0300

    pasta modelo

---
+++ contas/AppContas.java
@@ -0,0 +1,29 @@
+//import modelo.Conta;
+//import modelo.ContaCorrente;
+import modelo.ContaEspecial;
+import modelo.ContaPoupanca;
+
+public class AppContas {
+    public static void main(String[] args) {
+        ContaPoupanca conta = new ContaPoupanca(123); // tipo Conta
+
+        conta.setTaxaOperacao(0.1);
+
+        System.out.println(conta);
+
+        conta.deposito(200); // metodo
+        System.out.println(conta);
+
+        if (conta.saque(300)) {
+            System.out.println("Saque realizado.");
+        } else { 
+            System.out.println(" Saldo insuficiente.");
+
+        }
+        System.out.println(conta);
+        
+
+        
+        
+    }
+}
